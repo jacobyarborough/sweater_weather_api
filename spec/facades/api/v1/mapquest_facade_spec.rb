48127c6a7ab5297b9eea6c63de5759d335a883a2
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-16 17:01:48 -0700

    Refactor mapquest service spec

--- spec/facades/api/v1/mapquest_facade_spec.rb
+++ spec/facades/api/v1/mapquest_facade_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Mapquest Facade' do 
   let(:location) { 'denver, co' }
   it '.get_location_details', :vcr do 
-    lat_long = MapquestFacade.get_location_details(location)
+    response= MapquestFacade.get_location_details(location)
 
-    expect(lat_long). to be_a(string)
+    expect(response).to be_a(Hash)
   end 
 end 
--- spec/services/api/v1/mapquest_service_spec.rb
+++ spec/services/api/v1/mapquest_service_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe 'Mapquest Service' do
 
       expect(response).to be_a(Hash)
       expect(response[:results]).to be_an(Array)
-      expect(response[:results][0]).to have_key(:latLng)
-      expect(response[:results][0][latLng]).to be_a(Hash)
-      expect(response[:results][0][latLng]).to have_key(:lat)
-      expect(response[:results][0][latLng][:lat]).to be_a(Float)
-      expect(response[:results][0][latLng]).to have_key(:lng)
-      expect(response[:results][0][latLng][:lng]).to be_a(Float)
+      expect(response[:results][0]).to have_key(:locations)
+      expect(response[:results][0][:locations]).to be_an(Array)
+      expect(response[:results][0][:locations][0]).to have_key(:latLng)
+      expect(response[:results][0][:locations][0][:latLng]).to be_a(Hash)
+      expect(response[:results][0][:locations][0][:latLng]).to have_key(:lat)
+      expect(response[:results][0][:locations][0][:latLng][:lat]).to be_a(Float)
+      expect(response[:results][0][:locations][0][:latLng]).to have_key(:lng)
+      expect(response[:results][0][:locations][0][:latLng][:lng]).to be_a(Float)
     end 
   end 
 end 
