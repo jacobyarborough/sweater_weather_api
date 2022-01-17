6f38bd06079ed5e15248f0b2a658b4f263ca46fe
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-16 21:28:56 -0700

    Create weather poro test

--- app/facades/open_weather_facade.rb
+++ app/facades/open_weather_facade.rb
@@ -1,7 +1,6 @@
 class OpenWeatherFacade
   class << self 
     def get_weather(lat, lng)
-      binding.pry
       OpenWeatherService.get_weather_forecast(lat, lng)
     end 
   end 
--- spec/poros/weather_spec.rb
+++ spec/poros/weather_spec.rb
@@ -6,11 +6,38 @@ RSpec.describe Weather do
   it 'has attributes' do 
     expect(weather.id).to eq(null)
     expect(weather.type).to eq('forecast')
-    expect(weather.current_weather).to be_a(Hash)
-    expect(weather[:datetime]).to eq('datetimesting') #need to research format
-    expect(weather[:sunrise]).to eq('datetimesting') #need to research format
-    expect(weather[:sunset]).to eq('datetimesting') #need to research format
-    expect(weather[:temperature]).to eq()
 
+    expect(weather.current_weather).to be_a(Hash)
+    expect(weather.current_weather[:datetime]).to eq('datetimesting') #need to research format
+    expect(weather.current_weather[:sunrise]).to eq('datetimesting') #need to research format
+    expect(weather.current_weather[:sunset]).to eq('datetimesting') #need to research format
+    expect(weather.current_weather[:temperature]).to eq(31.75)
+    expect(weather.current_weather[:feels_like]).to eq(31.75)
+    expect(weather.current_weather[:humidity]).to eq(57)
+    expect(weather.current_weather[:uvi]).to eq(0)
+    expect(weather.current_weather[:visibility]).to eq(10000)
+    expect(weather.current_weather[:conditions]).to eq("few clouds")
+    expect(weather.current_weather[:icon]).to eq("02n")
+
+    expect(weather.daily_weather).to be_an(Array)
+    expect(weather.daily_weather.length).to eq(5)
+    expect(weather.daily_weather[0]).to be_a(Hash)
+    expect(weather.daily_weather[0].count).to eq(7)
+    expect(weather.daily_weather[0][:date]).to eq('datetimesting') #need to research format
+    expect(weather.daily_weather[0][:sunrise]).to eq('datetimesting') #need to research format
+    expect(weather.daily_weather[0][:sunset]).to eq('datetimesting') #need to research format
+    expect(weather.daily_weather[0][:max_temp]).to eq(47.66)
+    expect(weather.daily_weather[0][:min_temp]).to eq(31.75)
+    expect(weather.daily_weather[0][:conditions]).to eq("overcast clouds")
+    expect(weather.daily_weather[0][:icon]).to eq("04d")
+
+    expect(weather.hourly_weather).to be_an(Array)
+    expect(weather.hourly_weather.length).to eq(8)
+    expect(weather.hourly_weather[0]).to be_a(Hash)
+    expect(weather.hourly_weather[0].count).to eq(4)
+    expect(weather.hourly_weather[0][:time]).to eq('datetimesting') #need to research format
+    expect(weather.hourly_weather[0][:temperature]).to eq(32.7)
+    expect(weather.hourly_weather[0][:conditions]).to eq("few clouds")
+    expect(weather.hourly_weather[0][:icon]).to eq("02n")
   end 
 end 
