2988be51ad83e627d56e522bc4ba3886c9225131
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-17 21:36:56 -0700

    Refactor backgrounds controller

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::BackgroundsController < ApplicationController
   def show 
-    UnsplashFacade.get_image(params[:location])
+    if params[:location] == ' ' || params[:location] == ''
+      render json: {error: "Can't find location or location was entered incorrectly", status: 404}, status: 404
+    else 
+      render json: ImageSerializer.new(UnsplashFacade.get_image(params[:location]))
+    end 
   end 
 end
---
+++ app/serializers/image_serializer.rb
@@ -0,0 +1,7 @@
+class ImageSerializer 
+  include JSONAPI::Serializer 
+
+  set_id :id
+  set_type 'image'
+  attributes :image
+end 
