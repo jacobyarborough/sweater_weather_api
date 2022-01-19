cd90bb30b074f155ced72944ef0d94c9aee4c230
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-18 17:19:23 -0700

    Create auth token migration

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+  has_secure_token :auth_token
+  
+  def create
+    binding.pry
+    User.create!(email: user_params[:email], password: user_params[:password], password_confirmation: user_params[:password_confirmation])
+  end 
 end
+
+private 
+
+def user_params
+  JSON.parse(request.body.string, symbolize_names: :true)
+end 
