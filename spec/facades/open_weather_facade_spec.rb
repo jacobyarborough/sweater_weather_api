56700ff2e078079e536ba7612cea082f879aac4f
Jacob Yarborough <jayarborough95@gmail.com>
2022-01-19 00:13:07 -0700

    Refactor open weather facade

--- spec/facades/open_weather_facade_spec.rb
+++ spec/facades/open_weather_facade_spec.rb
@@ -3,9 +3,21 @@ require 'rails_helper'
 RSpec.describe 'OpenWeather Facade' do 
   let(:lat) { 39.738453 }
   let(:lng) { -104.984853 }
-  it '.get_weather', :vcr do 
-    response = OpenWeatherFacade.get_weather(lat, lng)
+  let(:time) { "15:45:32" }
 
-    expect(response).to be_a(Weather)
-  end 
+  describe '.get_weather' do 
+    it 'returns a weather object', :vcr do 
+      response = OpenWeatherFacade.get_weather(lat, lng)
+
+      expect(response).to be_a(Weather)
+    end 
+  end
+
+  describe '.get_weather_at_time' do 
+    it 'returns a weather object', :vcr do 
+      response = OpenWeatherFacade.get_weather_at_time(lat, lng, time)
+
+      expect(response).to be_a(WeatherTime)
+    end 
+  end
 end 
